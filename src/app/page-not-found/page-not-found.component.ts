6a32c9d89b0272631b4c2301c2f618fb3bd26b9b
sainadh43 <59512989+sainadh43@users.noreply.github.com>
2020-01-21 15:09:22 -0500

    Update page-not-found.component.ts

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,19 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+
+// here i am taking advantage of built in location service to go back to previous page
 
 @Component({
   selector: 'app-page-not-found',
-  template: `
-    <p>
-      page-not-found works!
-    </p>
-  `,
-  styles: []
+  templateUrl: './page-not-found.component.html',
+  styleUrls: ['./page-not-found.component.scss']
 })
 export class PageNotFoundComponent implements OnInit {
 
-  constructor() { }
+  public images = '../../assets/images/images.png';
+
+  constructor( private location: Location) {
+    setTimeout(() => {
+      this.location.back()
+    }, 20000)
+  }
 
   ngOnInit() {
   }
 
+  public goback(){
+    this.location.back();
+
+  }
 }
