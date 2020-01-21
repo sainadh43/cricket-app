08fe5c2083b10b8848e8333b4820a3747a5a60c8
sainadh43 <59512989+sainadh43@users.noreply.github.com>
2020-01-21 15:06:49 -0500

    Update dashboard.module.ts

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,39 +1,77 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { MatTabsModule, MatMenuModule } from "@angular/material";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+   MatTabsModule, MatFormFieldModule,MatToolbarModule, MatButtonModule ,
+   MatMenuModule, MatRadioModule,MatInputModule,MatCardModule,MatIconModule
+  } from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from "@angular/flex-layout";
 
-import { DashboardRoutingModule } from "./dashboard-routing.module";
-import { HomeComponent } from "./home/home.component";
-import { MatchesComponent } from "./matches/matches.component";
-import { StasticsComponent } from "./stastics/stastics.component";
-import { LoginComponent } from "./login/login.component";
-import { RegisterComponent } from "./register/register.component";
-import { PlayersComponent } from "./players/players.component";
-import { DashboardComponent } from "./dashboard.componenet";
-import { FixturesComponent } from "./matches/fixtures/fixtures.component";
-import { RecentResultsComponent } from "./matches/recent-results/recent-results.component";
-import { BowlingRecordsComponent } from "./stastics/bowling-records/bowling-records.component";
-import { BattingRecordsComponent } from "./stastics/batting-records/batting-records.component";
-import { FieldingRecordsComponent } from "./stastics/fielding-records/fielding-records.component";
-import { PlayerRankingsComponent } from "./stastics/player-rankings/player-rankings.component";
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardComponent} from './dashboard.component'
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { StasticsComponent } from './stastics/stastics.component';
+import { MatchesComponent } from './matches/matches.component';
+import { HomeComponent } from './home/home.component';
+import { PlayersComponent } from './players/players.component';
+import { BattingRecordsComponent } from './stastics/batting-records/batting-records.component';
+import { BowlingRecordsComponent } from './stastics/bowling-records/bowling-records.component';
+import { FieldingRecordsComponent } from './stastics/fielding-records/fielding-records.component';
+import { PlayerRankingsComponent } from './stastics/player-rankings/player-rankings.component';
+import { FixturesComponent } from './matches/fixtures/fixtures.component';
+import { RecentResultsComponent } from './matches/recent-results/recent-results.component';
+import { SearchPlayersComponent } from './players/search-players/search-players.component';
+import { AddPlayersComponent } from './players/add-players/add-players.component';
+import { ForgetPasswordComponent } from './forget-password/forget-password.component';
+
+
+
 
 @NgModule({
   declarations: [
-    HomeComponent,
-    MatchesComponent,
-    StasticsComponent,
     LoginComponent,
-    RegisterComponent,
-    PlayersComponent,
     DashboardComponent,
+    RegisterComponent,
+    StasticsComponent,
+    MatchesComponent,
+    HomeComponent,
+    PlayersComponent,
+    BattingRecordsComponent,
+    BowlingRecordsComponent,
+    FieldingRecordsComponent,
+    PlayerRankingsComponent,
     FixturesComponent,
     RecentResultsComponent,
-    BowlingRecordsComponent,
-    BattingRecordsComponent,
-    FieldingRecordsComponent,
-    PlayerRankingsComponent
+    SearchPlayersComponent,
+    AddPlayersComponent,
+    ForgetPasswordComponent
   ],
-  imports: [CommonModule, DashboardRoutingModule, MatTabsModule, MatMenuModule],
-  exports: [MatTabsModule, MatMenuModule]
+  imports: [
+    CommonModule,
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule,
+    DashboardRoutingModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatRadioModule,
+    MatCardModule,
+    MatIconModule
+  ],
+  // exports: [
+  //   MatTabsModule,
+  //   MatToolbarModule,
+  //   MatRadioModule,
+  //   MatButtonModule,
+  //   MatMenuModule,
+  //   MatFormFieldModule,
+  //   MatInputModule,
+  //   MatCardModule,
+  //   MatIconModule]
 })
-export class DashboardModule {}
+export class DashboardModule { }
