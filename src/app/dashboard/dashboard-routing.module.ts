4efa6a4db87f089dc89598f909a44ceb47081b78
sainadh43 <saireddyui@gmail.com>
2020-01-21 22:39:45 -0500

    new eidts commited

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,115 +1,113 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { DashboardComponent } from './dashboard.component';
-import { HomeComponent } from './home/home.component';
-import { MatchesComponent } from './matches/matches.component';
-import { PlayersComponent } from './players/players.component';
-import { RegisterComponent } from './register/register.component';
-import { StasticsComponent } from './stastics/stastics.component';
-import { FixturesComponent } from './matches/fixtures/fixtures.component';
-import { RecentResultsComponent } from './matches/recent-results/recent-results.component';
-import { BattingRecordsComponent } from './stastics/batting-records/batting-records.component';
-import { BowlingRecordsComponent } from './stastics/bowling-records/bowling-records.component';
-import { FieldingRecordsComponent } from './stastics/fielding-records/fielding-records.component';
-import { PlayerRankingsComponent } from './stastics/player-rankings/player-rankings.component';
-import { LoginComponent } from './login/login.component';
-import { SearchPlayersComponent } from './players/search-players/search-players.component';
-import { AddPlayersComponent } from './players/add-players/add-players.component';
-import { ForgetPasswordComponent } from './forget-password/forget-password.component';
-
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { DashboardComponent } from "./dashboard.component";
+import { HomeComponent } from "./home/home.component";
+import { MatchesComponent } from "./matches/matches.component";
+import { PlayersComponent } from "./players/players.component";
+import { RegisterComponent } from "./register/register.component";
+import { StasticsComponent } from "./stastics/stastics.component";
+import { FixturesComponent } from "./matches/fixtures/fixtures.component";
+import { RecentResultsComponent } from "./matches/recent-results/recent-results.component";
+import { BattingRecordsComponent } from "./stastics/batting-records/batting-records.component";
+import { BowlingRecordsComponent } from "./stastics/bowling-records/bowling-records.component";
+import { FieldingRecordsComponent } from "./stastics/fielding-records/fielding-records.component";
+import { PlayerRankingsComponent } from "./stastics/player-rankings/player-rankings.component";
+import { LoginComponent } from "./login/login.component";
+import { SearchPlayersComponent } from "./players/search-players/search-players.component";
+import { AddPlayersComponent } from "./players/add-players/add-players.component";
+import { ForgetPasswordComponent } from "./forget-password/forget-password.component";
 
 const routes: Routes = [
   {
-    path: '',
+    path: "",
     component: DashboardComponent,
     children: [
       {
-        path: 'home',
+        path: "home",
         component: HomeComponent
       },
       {
-        path:'forget-password',
+        path: "forget-password",
         component: ForgetPasswordComponent
       },
       {
-        path: 'matches',
+        path: "matches",
         component: MatchesComponent,
         children: [
           {
-            path: 'fixtures',
+            path: "fixtures",
             component: FixturesComponent
           },
           {
-            path: 'recent-results',
+            path: "recent-results",
             component: RecentResultsComponent
           },
           {
-            path: '**',
-            redirectTo: 'fixtures',
-            pathMatch: 'full'
+            path: "**",
+            redirectTo: "fixtures",
+            pathMatch: "full"
           }
         ]
       },
       {
-        path: 'players',
-        component: PlayersComponent ,
+        path: "players",
+        component: PlayersComponent,
         children: [
           {
-            path: 'search-players',
+            path: "search-players",
             component: SearchPlayersComponent
           },
           {
-            path: 'add-players',
+            path: "add-players",
             component: AddPlayersComponent
           },
           {
-            path: '**',
-            redirectTo: 'search-players',
-            pathMatch: 'full'
+            path: "**",
+            redirectTo: "search-players",
+            pathMatch: "full"
           }
         ]
       },
       {
-        path: 'register',
+        path: "register",
         component: RegisterComponent
       },
       {
-        path:'login',
+        path: "login",
         component: LoginComponent
       },
       {
-        path: 'stastics',
+        path: "stastics",
         component: StasticsComponent,
         children: [
           {
-            path: 'batting-records',
+            path: "batting-records",
             component: BattingRecordsComponent
           },
           {
-            path: 'bowling-records',
+            path: "bowling-records",
             component: BowlingRecordsComponent
           },
           {
-            path: 'fielding-records',
+            path: "fielding-records",
             component: FieldingRecordsComponent
           },
           {
-            path: 'player-rankings',
+            path: "player-rankings",
             component: PlayerRankingsComponent
           },
           {
-            path: '**',
-            redirectTo: 'batting-records',
-            pathMatch: 'full'
+            path: "**",
+            redirectTo: "batting-records",
+            pathMatch: "full"
           }
         ]
       },
-     {
-       path: '**',
-       redirectTo: 'home',
-       pathMatch: 'full'
-     }
+      {
+        path: "**",
+        redirectTo: "home",
+        pathMatch: "full"
+      }
     ]
   }
 ];
@@ -118,4 +116,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class DashboardRoutingModule { }
+export class DashboardRoutingModule {}
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,32 +1,36 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 import {
-   MatTabsModule, MatFormFieldModule,MatToolbarModule, MatButtonModule ,
-   MatMenuModule, MatRadioModule,MatInputModule,MatCardModule,MatIconModule
-  } from '@angular/material';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+  MatTabsModule,
+  MatFormFieldModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatMenuModule,
+  MatRadioModule,
+  MatInputModule,
+  MatCardModule,
+  MatIconModule
+} from "@angular/material";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { FlexLayoutModule } from "@angular/flex-layout";
 
-import { DashboardRoutingModule } from './dashboard-routing.module';
-import { DashboardComponent} from './dashboard.component'
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { StasticsComponent } from './stastics/stastics.component';
-import { MatchesComponent } from './matches/matches.component';
-import { HomeComponent } from './home/home.component';
-import { PlayersComponent } from './players/players.component';
-import { BattingRecordsComponent } from './stastics/batting-records/batting-records.component';
-import { BowlingRecordsComponent } from './stastics/bowling-records/bowling-records.component';
-import { FieldingRecordsComponent } from './stastics/fielding-records/fielding-records.component';
-import { PlayerRankingsComponent } from './stastics/player-rankings/player-rankings.component';
-import { FixturesComponent } from './matches/fixtures/fixtures.component';
-import { RecentResultsComponent } from './matches/recent-results/recent-results.component';
-import { SearchPlayersComponent } from './players/search-players/search-players.component';
-import { AddPlayersComponent } from './players/add-players/add-players.component';
-import { ForgetPasswordComponent } from './forget-password/forget-password.component';
-
-
-
+import { DashboardRoutingModule } from "./dashboard-routing.module";
+import { DashboardComponent } from "./dashboard.component";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
+import { StasticsComponent } from "./stastics/stastics.component";
+import { MatchesComponent } from "./matches/matches.component";
+import { HomeComponent } from "./home/home.component";
+import { PlayersComponent } from "./players/players.component";
+import { BattingRecordsComponent } from "./stastics/batting-records/batting-records.component";
+import { BowlingRecordsComponent } from "./stastics/bowling-records/bowling-records.component";
+import { FieldingRecordsComponent } from "./stastics/fielding-records/fielding-records.component";
+import { PlayerRankingsComponent } from "./stastics/player-rankings/player-rankings.component";
+import { FixturesComponent } from "./matches/fixtures/fixtures.component";
+import { RecentResultsComponent } from "./matches/recent-results/recent-results.component";
+import { SearchPlayersComponent } from "./players/search-players/search-players.component";
+import { AddPlayersComponent } from "./players/add-players/add-players.component";
+import { ForgetPasswordComponent } from "./forget-password/forget-password.component";
 
 @NgModule({
   declarations: [
@@ -62,7 +66,7 @@ import { ForgetPasswordComponent } from './forget-password/forget-password.compo
     MatRadioModule,
     MatCardModule,
     MatIconModule
-  ],
+  ]
   // exports: [
   //   MatTabsModule,
   //   MatToolbarModule,
@@ -74,4 +78,4 @@ import { ForgetPasswordComponent } from './forget-password/forget-password.compo
   //   MatCardModule,
   //   MatIconModule]
 })
-export class DashboardModule { }
+export class DashboardModule {}
